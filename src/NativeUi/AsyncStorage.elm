552c7840769dd7a2b12d6d9ed2cb92379131631a
Ian C. Anderson <ian@iancanderson.com>
2017-01-20 16:30:26 -0500

    Implement AsyncStorage.multiGet
    This implements `AsyncStorage.multiGet`, and returns a `Dict String
    (Maybe String)`, which is more convenient to use than React Native's
    list of lists.

--- src/NativeUi/AsyncStorage.elm
+++ src/NativeUi/AsyncStorage.elm
@@ -1,28 +1,39 @@
-module NativeUi.AsyncStorage exposing (Error, getItem, setItem, removeItem)
+module NativeUi.AsyncStorage exposing (Error, getItem, setItem, removeItem, multiGet)
 
 {-| elm-native-ui AsyncStorage
 
-@docs Error, setItem, getItem, removeItem
+@docs Error, setItem, getItem, removeItem, multiGet
 -}
 
 import Native.NativeUi.AsyncStorage
-
 import Task exposing (Task)
+import Dict exposing (Dict, fromList)
+
 
 {-| -}
-type Error = Error String
+type Error
+    = Error String
+
 
 {-| -}
 getItem : String -> Task Error (Maybe String)
 getItem =
-  Native.NativeUi.AsyncStorage.getItem
+    Native.NativeUi.AsyncStorage.getItem
+
 
 {-| -}
 setItem : String -> String -> Task Error ()
 setItem =
-  Native.NativeUi.AsyncStorage.setItem
+    Native.NativeUi.AsyncStorage.setItem
+
 
 {-| -}
 removeItem : String -> Task Error ()
 removeItem =
-  Native.NativeUi.AsyncStorage.removeItem
+    Native.NativeUi.AsyncStorage.removeItem
+
+
+{-| -}
+multiGet : List String -> Task Error (Dict String (Maybe String))
+multiGet strings =
+    Task.map Dict.fromList (Native.NativeUi.AsyncStorage.multiGet strings)
