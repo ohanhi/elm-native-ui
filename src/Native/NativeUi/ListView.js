0d5871c62ae61c4a27228a3c083243495092e51b
Josh Steiner <josh@jsteiner.me>
2017-02-24 10:23:27 -0500

    Add initial ListView implementation
    Due to the imperative nature of ListView, this implementation doesn't
    really work well with the Elm architecture, forcing you to manually keep
    a DataSource up to date.
    
    An experimental alternative called
    [FlatList](https://medium.com/@cooperka/react-native-new-flatlist-component-30db558c7a5b#.3kodtbtz7)
    was recently added to ReactNative which is likely a better long term
    fit, but this works in the meantime.
    
    Example use:
    
        type alias Model =
          { wombats : List Wombat
          , wombatDataSource : DataSource Wombat
          }
    
        view model =
          listView
            model.wombatDataSource
            wombatView
            [ Ui.style
                [ Style.backgroundColor Color.white
                ]
            ]
    
        update msg model =
          LoadWombats wombats ->
            let
                wombatDataSource = updateDataSource model.wombatDataSource wombats
            in
              ( { model | wombats = wombats, wombatDataSource = wombatDataSource }, Cmd.none )

---
+++ src/Native/NativeUi/ListView.js
@@ -0,0 +1,28 @@
+const _ohanhi$elm_native_ui$Native_NativeUi_ListView = function () {
+  const React = require('react');
+  const { ListView, Text } = require('react-native');
+
+  const emptyDataSource = new ListView.DataSource({
+    rowHasChanged: function (a, b) { return a !== b; }
+  });
+
+  function unencodedProperty(dataSource) {
+    return {
+      type: 'prop',
+      propName: 'dataSource',
+      value: dataSource
+    };
+  }
+
+  function updateDataSource(dataSource, data) {
+    var result = _elm_lang$core$Native_List.toArray(data);
+    return dataSource.cloneWithRows(result);
+  }
+
+  return {
+    view: ListView,
+    emptyDataSource: emptyDataSource,
+    unencodedProperty: unencodedProperty,
+    updateDataSource: F2(updateDataSource),
+  };
+}();
