aca73c1f62648d4ec902a8f52b9b0efe1b7ec38b
Ian C. Anderson <ian@iancanderson.com>
2017-06-23 19:12:47 -0400

    Initial implementation for NativeUi.Alert

---
+++ src/NativeUi/Alert.elm
@@ -0,0 +1,23 @@
+module NativeUi.Alert exposing (Error, alert)
+
+{-|
+@docs Error, alert
+-}
+
+import Native.NativeUi.Alert
+import Task exposing (Task)
+
+
+{-| -}
+type Error
+    = Error String
+
+
+type alias AlertButton =
+    ( String, Bool )
+
+
+{-| -}
+alert : String -> String -> List AlertButton -> Task Error Bool
+alert =
+    Native.NativeUi.Alert.alert
